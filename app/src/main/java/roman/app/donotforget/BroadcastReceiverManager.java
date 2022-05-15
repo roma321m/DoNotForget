52618cb62586b09d9afc94fa73e731c14a0f2a13
Roman Michailov <roma321m@gmail.com>
2022-05-15 14:20:15 +0300

    Added BroadcastReceiver to start the service after a device reboot.

---
+++ app/src/main/java/roman/app/donotforget/BroadcastReceiverManager.java
@@ -0,0 +1,16 @@
+package roman.app.donotforget;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BroadcastReceiverManager extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)){
+            Intent serviceIntent = new Intent(context, ForegroundServiceManager.class);
+            context.startForegroundService(serviceIntent);
+        }
+    }
+}
