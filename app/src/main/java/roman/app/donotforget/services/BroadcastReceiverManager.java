60f8bf31e04d2e1d1e2e1de60b70714e97ab3c51
Roman Michailov <roma321m@gmail.com>
2022-05-15 18:16:56 +0300

    Fixed a problem after device reboot.

--- app/src/main/java/roman/app/donotforget/services/BroadcastReceiverManager.java
+++ app/src/main/java/roman/app/donotforget/services/BroadcastReceiverManager.java
@@ -4,11 +4,16 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import roman.app.donotforget.data.DataManager;
+import roman.app.donotforget.data.SQLiteDBManager;
+
 public class BroadcastReceiverManager extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)){
+            SQLiteDBManager.initHelper(context);
+            DataManager.initHelper(context);
             Intent serviceIntent = new Intent(context, ForegroundServiceManager.class);
             context.startForegroundService(serviceIntent);
         }
